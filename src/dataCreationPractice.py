1582f7006b87cf17de678f5f5435204fac5d3c4d
Nikhil Raman <n_raman@yahoo.com>
2016-12-11 04:36:57 -0500

    setup

--- src/dataCreationPractice.py
+++ src/dataCreationPractice.py
@@ -25,4 +25,38 @@ import sys
 # 6. Round each value to nearest integer and create histogram of values for magnitude -> this becomes vector of length n1 
 # 		 	--> where n1 is 300 (or 256 or 200 or the actual max)
 # 7. Round each value to nearest integer and create histogram of values for direction -> this becomes vector of length n2 
-#			--> where n2 is 360 (ranging from -180 to 180 for degrees)
+#			--> where n2 is 360 (ranging from -180 to 180 for degrees) 
+# 8. Concatenate the two vectors and we have our desired feature vector 
+
+
+if __name__ == "__main__": 
+
+	# Main code  
+
+	### Print the command line arguments for verifcation ###
+	print 'Number of arguments:', len(sys.argv), 'arguments.'
+	print 'Argument List:', str(sys.argv), '\n' 
+
+	infile = sys.argv[1] 
+
+	### Open the input file and convert to matrix of pixels ###
+
+	img = None
+	try:
+		img = Image.open(infile)
+	except IOError:
+		print "Invalid file!", infile
+		print "Please Enter a command of the form: "
+		print "\n'python imageSegmentation.py K inputImageFilename outputImageFilename'\n"
+		sys.exit() 
+
+
+	X = np.asarray(list(img.getdata()))
+	width, height = img.size   
+
+	print X
+
+
+
+
+
